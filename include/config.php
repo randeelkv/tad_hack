1b563297d63211ec1219c93b74d77e836fcabc35
DimuthuLakmal <kjtdimuthu@gmail.com>
2016-09-27 01:29:53 +0530

    Doctor Signup added

--- include/config.php
+++ include/config.php
@@ -5,6 +5,6 @@
  */
 define("DB_HOST", "localhost"); //write your host
 define("DB_USER", "root"); //write your db username
-define("DB_PASSWORD", ""); //write your db password
+define("DB_PASSWORD", "ijse"); //write your db password
 define("DB_DATABASE", "tad_med"); //write your database name
 ?>
